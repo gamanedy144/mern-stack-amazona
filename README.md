4e98e11b4eca4378b17e0e2ee6f98a350bb7a5bb
gamanedy144 <gamanedy@yahoo.com>
2022-06-26 14:37:51 +0300

    Create PlaceOrder Screen

--- README.md
+++ README.md
@@ -117,3 +117,7 @@ Mern Stack eCommerce application
 26. Create Payment Screen
     1. create input forms
     2. handle submit
+27. Create Place Order Screen
+    1. show cart items, payment and address
+    2. handle place order action
+    3. create order create api
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import SignInScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -81,6 +82,7 @@ function App() {
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
               <Route path="/payment" element={<PaymentMethodScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
             </Routes>
           </Container>
         </main>
